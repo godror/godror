3e66fc2c405f068fc0eada885176ecfcbb7ac0bf
Tamás Gulácsi <tgulacsi78@gmail.com>
2018-09-11 18:21:32 +0200

    bump version to 2.7.0

--- version.go
+++ version.go
@@ -1,8 +1,8 @@
 package goracle
 
-//go:generate bash -c "set -x; { curl -L https://github.com/oracle/odpi/archive/v2.4.2.tar.gz || wget -O - https://github.com/oracle/odpi/archive/v2.4.2.zip; } | tar xzvf - odpi-2.4.2/{embed,include,src,CONTRIBUTING.md,LICENSE.md,README.md}"
+//go:generate bash -c "set -x; { curl -L https://github.com/oracle/odpi/archive/v3.0.0.tar.gz || wget -O - https://github.com/oracle/odpi/archive/v3.0.0.zip; } | tar xzvf - odpi-3.0.0/{embed,include,src,CONTRIBUTING.md,LICENSE.md,README.md}"
 //go:generate rm -rf odpi
-//go:generate mv odpi-2.4.2 odpi
+//go:generate mv odpi-3.0.0 odpi
 
 // Version of this driver
-const Version = "v2.6.1"
+const Version = "v2.7.0"
