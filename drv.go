523144da02a89ffa67fd5813f74ed928c20da60c
Tamás Gulácsi <tgulacsi78@gmail.com>
2019-06-26 18:21:11 +0200

    UTC offset is always 0

--- drv.go
+++ drv.go
@@ -908,22 +908,18 @@ func parseTZ(s string) (int, error) {
 		}
 		s = s[:i]
 	}
-	if i := strings.IndexByte(s, '/'); i >= 0 {
-		destLoc, err := time.LoadLocation("UTC")
-		if err != nil {
-			return tz, errors.Wrap(err,s)
-		}
-		targetLoc, err := time.LoadLocation(s)
-		if err != nil {
-			return tz, errors.Wrap(err,s)
-		}
+	if tz == 0 {
+		if i := strings.IndexByte(s, '/'); i >= 0 {
+			targetLoc, err := time.LoadLocation(s)
+			if err != nil {
+				return tz, errors.Wrap(err, s)
+			}
 
-		now := time.Now()
-		_, destOffset := now.In(destLoc).Zone()
-		_, localOffset := now.In(targetLoc).Zone()
+			_, localOffset := time.Now().In(targetLoc).Zone()
 
-		tz+= localOffset-destOffset
-		return tz, nil
+			tz = localOffset
+			return tz, nil
+		}
 	}
 	if i64, err := strconv.ParseInt(s, 10, 5); err != nil {
 		return tz, errors.Wrap(err, s)
