7a10a68f5bfe6af8a389814a27303bd421bb5092
Denis Loginov <denis@broadinstitute.org>
2020-02-22 12:45:26 -0500

    Incorporate Callback into SubscriptionParams

--- orahlp.go
+++ orahlp.go
@@ -479,7 +479,7 @@ type Conn interface {
 	ClientVersion() (VersionInfo, error)
 	ServerVersion() (VersionInfo, error)
 	GetObjectType(name string) (ObjectType, error)
-	NewSubscriptionWithParams(SubscriptionParams, func(Event)) (*Subscription, error)
+	NewSubscriptionWithParams(SubscriptionParams) (*Subscription, error)
 	NewSubscription(string, func(Event)) (*Subscription, error)
 	Startup(StartupMode) error
 	Shutdown(ShutdownMode) error
