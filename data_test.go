036e37d416ca1485a9dbeba9b3a85ea8c8e7a780
Steve Fan <29133953+stevefan1999-personal@users.noreply.github.com>
2020-07-21 15:59:58 +0800

    add some tests

--- data_test.go
+++ data_test.go
@@ -19,6 +19,34 @@ func TestDataSetGet(t *testing.T) {
 		}
 	}
 
+	for _, want := range []int8{-126, 126} {
+		d.Set(want)
+		if got := d.Get(); got.(int8) != want {
+			t.Errorf("set %v, got %v", want, got)
+		}
+	}
+
+	for _, want := range []int16{-30000, -15000, 0, 15000, 30000} {
+		d.Set(want)
+		if got := d.Get(); got.(int16) != want {
+			t.Errorf("set %v, got %v", want, got)
+		}
+	}
+
+	for _, want := range []uint8{0, 128, 253} {
+		d.Set(want)
+		if got := d.Get(); got.(uint8) != want {
+			t.Errorf("set %v, got %v", want, got)
+		}
+	}
+
+	for _, want := range []uint16{0, 15000, 30000, 45000, 60000} {
+		d.Set(want)
+		if got := d.Get(); got.(uint16) != want {
+			t.Errorf("set %v, got %v", want, got)
+		}
+	}
+
 	for _, want := range []bool{true, false} {
 		d.SetBool(want)
 		if got := d.GetBool(); got != want {
