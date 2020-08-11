57c99d6a7865e3e74063bf40efdcef6dfcee6dee
Tamás Gulácsi <tgulacsi78@gmail.com>
2020-08-11 10:04:46 +0200

    doc/deploy.sh: fix trap typo

--- doc/deploy.sh
+++ doc/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eu
 git checkout gh-pages
-trap EXIT 'git checkout master'
+trap 'git checkout master' EXIT 
 version="$(git tag | sort -Vr|head -n1)"
 git checkout "$version" -- $({ git show "$version":./doc; git show "$version":./; } | grep '[.]md$')
 if git status --porcelain | grep -q '^[ MAD]'; then
