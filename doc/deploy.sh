94f87227a6e8ead21e8bc5fd1a5a9d6ca6b7b4e3
Tamás Gulácsi <tgulacsi78@gmail.com>
2020-08-11 10:26:58 +0200

    doc/deploy.sh: Fix checkout paths

--- doc/deploy.sh
+++ doc/deploy.sh
@@ -3,7 +3,8 @@ set -eu
 git checkout gh-pages
 trap 'git checkout master' EXIT 
 version="$(git tag | sort -Vr|head -n1)"
-git checkout "$version" -- $({ git show "$version":./doc; git show "$version":./; } | grep '[.]md$')
+set -x
+git checkout "$version" -- $(git show "$version":./doc | sed -ne '/[.]md$/ s,^,./doc/,p'; git show "$version":./ | grep '[.]md$')
 if git status --porcelain | grep -q '^[ MAD]'; then
 	git commit -am "Update gh-pages to $version"
 	git push origin gh-pages
