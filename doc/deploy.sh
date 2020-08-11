e0c30509bb7a622b525096e62b57e3d30f62af8a
Tamás Gulácsi <tgulacsi78@gmail.com>
2020-08-11 10:02:50 +0200

    doc/deploy.sh: *.md is not enough

--- doc/deploy.sh
+++ doc/deploy.sh
@@ -2,7 +2,7 @@
 set -eu
 git checkout gh-pages
 version="$(git tag | sort -Vr|head -n1)"
-git checkout "$version" -- *.md doc/*.md
+git checkout "$version" -- $(git show "$version":./doc; git show "$version":./ | grep '[.]md$')
 if git status --porcelain | grep -q '^[ MAD]'; then
 	git commit -am "Update gh-pages to $version"
 	git push origin gh-pages
