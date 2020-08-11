59d6643410122761d900da4a16099df8583b5c2b
Tamás Gulácsi <tgulacsi78@gmail.com>
2020-08-11 10:03:52 +0200

    deploy: grep .md

--- doc/deploy.sh
+++ doc/deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 set -eu
 git checkout gh-pages
+trap EXIT 'git checkout master'
 version="$(git tag | sort -Vr|head -n1)"
-git checkout "$version" -- $(git show "$version":./doc; git show "$version":./ | grep '[.]md$')
+git checkout "$version" -- $({ git show "$version":./doc; git show "$version":./; } | grep '[.]md$')
 if git status --porcelain | grep -q '^[ MAD]'; then
 	git commit -am "Update gh-pages to $version"
 	git push origin gh-pages
 fi
-git checkout master
