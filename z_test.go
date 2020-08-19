5c76d6fd4162c7bbd278189f4c814a9169b5425f
Sudarshan Soma <sudarshan.ss.s@oracle.com>
2020-08-19 01:18:02 -0700

    modified the name of defaultFetchValue to useDefaultFetchValue

--- z_test.go
+++ z_test.go
@@ -48,7 +48,7 @@ var (
 var tblSuffix string
 
 const maxSessions = 16
-const defaultFetchValue = -99
+const useDefaultFetchValue = -99
 
 func init() {
 	hsh := fnv.New32()
@@ -3316,20 +3316,20 @@ func TestPreFetchQuery(t *testing.T) {
 		pf, as   int
 		srt, mrt uint
 	}{
-		{defaultFetchValue, defaultFetchValue, 1, 4},
-		{0, defaultFetchValue, 2, 4},
-		{1, defaultFetchValue, 2, 4},
-		{2, defaultFetchValue, 1, 4},
-		{100, defaultFetchValue, 1, 3},
+		{useDefaultFetchValue, useDefaultFetchValue, 1, 4},
+		{0, useDefaultFetchValue, 2, 4},
+		{1, useDefaultFetchValue, 2, 4},
+		{2, useDefaultFetchValue, 1, 4},
+		{100, useDefaultFetchValue, 1, 3},
 		{-1, 100, 2, 4},
 		{0, 100, 2, 4},
 		{1, 100, 2, 4},
 		{2, 100, 1, 4},
 		{100, 100, 1, 3},
-		{defaultFetchValue, 40, 1, 7},
+		{useDefaultFetchValue, 40, 1, 7},
 		{2, 40, 1, 7},
 		{-1, 40, 2, 7},
-		{120, defaultFetchValue, 1, 3},
+		{120, useDefaultFetchValue, 1, 3},
 		{120, 100, 1, 3},
 		{120, 0, 1, 3},
 		{120, -1, 1, 3},
@@ -3338,7 +3338,7 @@ func TestPreFetchQuery(t *testing.T) {
 		{10, 10, 1, 22},
 		{214, 214, 1, 2},
 		{215, 214, 1, 1},
-		{215, defaultFetchValue, 1, 1},
+		{215, useDefaultFetchValue, 1, 1},
 		{215, 10, 1, 1},
 	} {
 		srt, mrt := runPreFetchTests(t, sid(), tCase.pf, tCase.as)
@@ -3390,11 +3390,11 @@ func singleRowFetch(t *testing.T, pf int, as int) uint {
 	tbl := "t_employees" + tblSuffix
 	query := "select employee_id from " + tbl + " where employee_id = :id"
 
-	if pf == defaultFetchValue && as == defaultFetchValue {
+	if pf == useDefaultFetchValue && as == useDefaultFetchValue {
 		err = testDb.QueryRowContext(ctx, query, 100).Scan(&employeeid)
-	} else if pf == defaultFetchValue && as != defaultFetchValue {
+	} else if pf == useDefaultFetchValue && as != useDefaultFetchValue {
 		err = testDb.QueryRowContext(ctx, query, 100, godror.FetchArraySize(as)).Scan(&employeeid)
-	} else if pf != defaultFetchValue && as == defaultFetchValue {
+	} else if pf != useDefaultFetchValue && as == useDefaultFetchValue {
 		err = testDb.QueryRowContext(ctx, query, 100, godror.PrefetchCount(pf)).Scan(&employeeid)
 	} else {
 		err = testDb.QueryRowContext(ctx, query, 100, godror.PrefetchCount(pf), godror.FetchArraySize(as)).Scan(&employeeid)
@@ -3414,11 +3414,11 @@ func multiRowFetch(t *testing.T, pf int, as int) uint {
 	var rows *sql.Rows
 	var err error
 
-	if pf == defaultFetchValue && as == defaultFetchValue {
+	if pf == useDefaultFetchValue && as == useDefaultFetchValue {
 		rows, err = testDb.QueryContext(ctx, query)
-	} else if pf == defaultFetchValue && as != defaultFetchValue {
+	} else if pf == useDefaultFetchValue && as != useDefaultFetchValue {
 		rows, err = testDb.QueryContext(ctx, query, godror.FetchArraySize(as))
-	} else if pf != defaultFetchValue && as == defaultFetchValue {
+	} else if pf != useDefaultFetchValue && as == useDefaultFetchValue {
 		rows, err = testDb.QueryContext(ctx, query, godror.PrefetchCount(pf))
 	} else {
 		rows, err = testDb.QueryContext(ctx, query, godror.PrefetchCount(pf), godror.FetchArraySize(as))
