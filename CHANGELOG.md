2e78b2b99be8b5038d3c11f5887200736ff7e6e5
Tamás Gulácsi <tamas@gulacsi.eu>
2020-10-16 12:20:52 +0200

    Bump version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,10 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+## [0.20.5]
+### Changed
+- Obey context deadlines everyewhere by calling OCIBreak on timeout/cancelation.
+
 ## [0.20.1]
 ### Changed
 - Fix Break (context cancelation/timeout) handling for good.
--- version.go
+++ version.go
@@ -7,4 +7,4 @@ package godror
 //go:generate bash -c "echo 4.0.2>odpi-version; set -x; curl -L https://github.com/oracle/odpi/archive/$(echo -n v; cat odpi-version).tar.gz | tar xzvf - odpi-$(cat odpi-version)/{embed,include,src,CONTRIBUTING.md,LICENSE.md,README.md} && cp -a odpi/embed/require.go odpi-$(cat odpi-version)/embed/ && cp -a odpi/include/require.go odpi-$(cat odpi-version)/include/ && cp -a odpi/src/require.go odpi-$(cat odpi-version)/src/ && rm -rf odpi && mv odpi-$(cat odpi-version) odpi; rm -f odpi-{,v}version; git checkout -- $(git status --porcelain -- odpi/*/*.go | sed -n -e '/ D / { s/^ D //;p;}')"
 
 // Version of this driver
-const Version = "v0.20.3"
+const Version = "v0.20.5"
