cf1bc06391f35d84bfb8d933b51c23e6973325be
Tamás Gulácsi <tamas@gulacsi.eu>
2020-12-06 10:32:24 +0100

    Benchmark first record time, too

--- z_bench_113_test.go
+++ z_bench_113_test.go
@@ -7,6 +7,7 @@ package godror_test
 
 import (
 	"context"
+	"database/sql"
 	"strconv"
 	"testing"
 	"time"
@@ -20,6 +21,7 @@ func BenchmarkSelect113(b *testing.B) {
 	defer cancel()
 	const tbl = "test_bench_113"
 	{
+		testDb.ExecContext(ctx, "DROP TABLE "+tbl)
 		const qry = "CREATE TABLE " + tbl + " (F_cust_id NUMBER(19) NOT NULL, F_email VARCHAR2(255) NOT NULL, F_email_id NUMBER(19) NOT NULL)"
 		if _, err := testDb.ExecContext(ctx, qry); err != nil {
 			b.Fatalf("%s: %+v", qry, err)
@@ -51,7 +53,8 @@ func BenchmarkSelect113(b *testing.B) {
 		b.Logf("Inserted %d records into %s in %s.", n, tbl, time.Since(t))
 	}
 
-	const qry = "SELECT F_cust_id, F_email, F_email_id FROM " + tbl
+	const qry = "SELECT F_cust_id, F_email, F_email_id FROM " + tbl + " ORDER BY DBMS_RANDOM.RANDOM"
+	b.Log(qry)
 	b.StartTimer()
 	b.Run("simple", func(b *testing.B) {
 		for i := 0; i < b.N; i++ {
@@ -59,43 +62,52 @@ func BenchmarkSelect113(b *testing.B) {
 			if err != nil {
 				b.Fatalf("%s: %+v", qry, err)
 			}
-			var n uint64
-			//t := time.Now()
-			for rows.Next() {
-				var id, emailID uint64
-				var email string
-				if err := rows.Scan(&id, &email, &emailID); err != nil {
-					rows.Close()
-					b.Fatalf("%s: %+v", qry, err)
-				}
-				n++
-			}
-			rows.Close()
-			b.ReportMetric(float64(n), "record")
-			//b.Logf("Selected %d records in %s.", n, time.Since(t))
+			fetchRows(b, rows, (i+1)*100, i == 0)
 		}
 	})
 
-	b.Run("prefetch-8192", func(b *testing.B) {
-		for i := 0; i < b.N; i++ {
-			rows, err := testDb.QueryContext(ctx, qry, godror.FetchArraySize(8192), godror.PrefetchCount(8193))
-			if err != nil {
-				b.Fatalf("%s: %+v", qry, err)
-			}
-			var n uint64
-			//t := time.Now()
-			for rows.Next() {
-				var id, emailID uint64
-				var email string
-				if err := rows.Scan(&id, &email, &emailID); err != nil {
-					rows.Close()
+	for i := 2; i < 8192; i *= 8 {
+		arraySize := i
+		b.Run("prefetch-"+strconv.Itoa(i), func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				rows, err := testDb.QueryContext(ctx, qry, godror.FetchArraySize(arraySize), godror.PrefetchCount(arraySize+1))
+				if err != nil {
 					b.Fatalf("%s: %+v", qry, err)
 				}
-				n++
+				fetchRows(b, rows, (i+1)*100, i == 0)
 			}
+		})
+	}
+}
+
+func fetchRows(b *testing.B, rows *sql.Rows, maxRows int, first bool) {
+	if maxRows == 0 {
+		rows.Close()
+		return
+	}
+	defer rows.Close()
+	var n uint64
+	var t time.Time
+	if first {
+		t = time.Now()
+	}
+	for rows.Next() {
+		var id, emailID uint64
+		var email string
+		if err := rows.Scan(&id, &email, &emailID); err != nil {
 			rows.Close()
-			b.ReportMetric(float64(n), "record")
-			//b.Logf("Selected %d records in %s.", n, time.Since(t))
+			b.Fatalf("%+v", err)
 		}
-	})
+		if first && !t.IsZero() {
+			b.ReportMetric(float64(time.Since(t)/time.Microsecond), "firstRecordMicros")
+			t = time.Time{}
+		}
+		n++
+		if n == uint64(maxRows) {
+			break
+		}
+	}
+	rows.Close()
+	b.ReportMetric(float64(n), "record")
+	//b.Logf("Selected %d records in %s.", n, time.Since(t))
 }
