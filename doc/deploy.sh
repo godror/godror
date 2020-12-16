3d721a2c342460a140e7f6fe9a27692efbbf38ea
Tamás Gulácsi <tamas@gulacsi.eu>
2020-12-16 12:41:43 +0100

    doc/deploy: Pull from upstream first

--- doc/deploy.sh
+++ doc/deploy.sh
@@ -2,9 +2,12 @@
 set -eu
 git checkout gh-pages
 trap 'git checkout master' EXIT 
+git pull -s recursive -X theirs origin gh-pages
 version="$(git tag | sort -Vr|head -n1)"
 set -x
-git checkout "$version" -- $(git show "$version":./doc | sed -ne '/[.]md$/ s,^,./doc/,p'; git show "$version":./ | grep '[.]md$')
+files=$(git show "$version":./doc | sed -ne '/[.]md$/ s,^,./doc/,p'; git show "$version":./ | grep '[.]md$')
+git checkout "$version" -- $files
+git add $files
 if git status --porcelain | grep -q '^[ MAD]'; then
 	git commit -am "Update gh-pages to $version"
 	git push origin gh-pages
