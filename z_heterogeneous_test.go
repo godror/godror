4f805fabc617ec82e947ba03d82e2ea6a23426b5
Sudarshan <48428602+sudarshan12s@users.noreply.github.com>
2021-02-07 21:31:41 +0530

    Add support for Proxy Support in external authentication

--- z_heterogeneous_test.go
+++ z_heterogeneous_test.go
@@ -16,6 +16,19 @@ import (
 	godror "github.com/godror/godror"
 )
 
+// Following are covered
+// - standalone=0
+//   - heterogeneous pool create with username, password and Query
+//     without username, password
+//   - Query from pool with session-username (proxyUser) ,
+//     session-password(proxyPassword)
+//   - Query from pool with session-username (proxyUser) and without
+//     session-password
+//   - Query from pool with session-username (proxyUser) enclosed
+//     under brackets for failure test
+// - standalone=1
+//   - create connection with username, password
+
 func TestHeterogeneousPoolIntegration(t *testing.T) {
 	t.Parallel()
 	ctx, cancel := context.WithTimeout(testContext("HeterogeneousPoolIntegration"), 30*time.Second)
@@ -28,7 +41,9 @@ func TestHeterogeneousPoolIntegration(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	cs.Heterogeneous = true
+	if !cs.IsStandalone() {
+		cs.Heterogeneous = true
+	}
 	username := cs.Username
 	testHeterogeneousConStr := cs.StringWithPassword()
 	t.Log(testHeterogeneousConStr)
@@ -70,26 +85,93 @@ func TestHeterogeneousPoolIntegration(t *testing.T) {
 		In   context.Context
 		Want string
 	}{
-		"noContext":       {In: ctx, Want: username},
-		"proxyUser":       {In: godror.ContextWithUserPassw(ctx, proxyUser, proxyPassword, ""), Want: proxyUser},
-		"proxyUserNoPass": {In: godror.ContextWithUserPassw(ctx, proxyUser, "", ""), Want: proxyUser},
+		"noContext":             {In: ctx, Want: username},
+		"proxyUser":             {In: godror.ContextWithUserPassw(ctx, proxyUser, proxyPassword, ""), Want: proxyUser},
+		"proxyUserNoPass":       {In: godror.ContextWithUserPassw(ctx, proxyUser, "", ""), Want: proxyUser},
+		"proxyUserwithBrackets": {In: godror.ContextWithUserPassw(ctx, "["+proxyUser+"]", "", ""), Want: proxyUser},
 	}
 	if cs.IsStandalone() {
+		delete(testCases, "proxyUser")
 		delete(testCases, "proxyUserNoPass")
+		delete(testCases, "proxyUserwithBrackets")
 	}
 	for tName, tCase := range testCases {
 		t.Run(tName, func(t *testing.T) {
 			var result string
 			if err = testHeterogeneousDB.QueryRowContext(tCase.In, "SELECT user FROM dual").Scan(&result); err != nil {
-				t.Fatalf("%s: %+v", tName, err)
+				if tName == "proxyUserwithBrackets" {
+					if !strings.Contains(err.Error(), "ORA-00987:") {
+						t.Errorf("%s: unexpected Error %s", tName, err.Error())
+					}
+				} else {
+					t.Fatalf("%s: %+v", tName, err)
+				}
 			}
-			if !strings.EqualFold(tCase.Want, result) {
-				t.Errorf("%s: currentUser got %s, wanted %s", tName, result, tCase.Want)
+			if tName != "proxyUserwithBrackets" {
+				if !strings.EqualFold(tCase.Want, result) {
+					t.Errorf("%s: currentUser got %s, wanted %s", tName, result, tCase.Want)
+				}
 			}
 		})
 
 	}
 
+}
+
+// passing Proxyuser at the time of Go pool creation
+// user = proxyusername[sessionusername], password for proxyusername
+func TestHeterogeneousConnCreationWithProxy(t *testing.T) {
+	t.Parallel()
+	ctx, cancel := context.WithTimeout(testContext("HeterogeneousConnCreationWithProxy"), 30*time.Second)
+	defer cancel()
+
+	const sessionUserPassword = "myPassword666myPassword"
+	const sessionUser = "test_hetero_create_sessionUser"
+
+	cs, err := godror.ParseDSN(testConStr)
+	if err != nil {
+		t.Fatal(err)
+	}
+	username := cs.Username
+	if !cs.IsStandalone() {
+		cs.Heterogeneous = true
+	}
+	cs.Username += "[" + sessionUser + "]"
+	testHeterogeneousConStr := cs.StringWithPassword()
+	t.Log(testHeterogeneousConStr)
+
+	var testHeterogeneousDB *sql.DB
+	if testHeterogeneousDB, err = sql.Open("godror", testHeterogeneousConStr); err != nil {
+		t.Fatal(fmt.Errorf("%s: %w", testHeterogeneousConStr, err))
+	}
+	defer testHeterogeneousDB.Close()
+	testHeterogeneousDB.SetMaxIdleConns(0)
+
+	testDb.ExecContext(ctx, fmt.Sprintf("DROP USER %s", sessionUser))
+
+	for _, qry := range []string{
+		fmt.Sprintf("CREATE USER %s IDENTIFIED BY "+sessionUserPassword, sessionUser),
+		fmt.Sprintf("GRANT CREATE SESSION TO %s", sessionUser),
+		fmt.Sprintf("ALTER USER %s GRANT CONNECT THROUGH %s", sessionUser, username),
+	} {
+		if _, err = testDb.ExecContext(ctx, qry); err != nil {
+			if strings.Contains(err.Error(), "ORA-01031:") {
+				t.Log("Please issue this:\nGRANT CREATE USER, DROP USER, ALTER USER TO " + username + ";\n" +
+					"GRANT CREATE SESSION TO " + username + " WITH ADMIN OPTION;\n")
+			}
+			t.Skip(fmt.Errorf("%s: %w", qry, err))
+		}
+	}
+	defer func() {
+		testDb.ExecContext(testContext("HeterogeneousConnCreationWithProxy-drop"), "DROP USER "+sessionUser)
+	}()
+	var result string
+	if err = testHeterogeneousDB.QueryRowContext(ctx, "SELECT user FROM dual").Scan(&result); err != nil {
+		t.Fatalf("%+v", err)
+	}
+	if !strings.EqualFold(sessionUser, result) {
+		t.Errorf("currentUser got %s, wanted %s", result, sessionUser)
+	}
 }
 
 func TestContextWithUserPassw(t *testing.T) {
@@ -100,7 +182,9 @@ func TestContextWithUserPassw(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	cs.Heterogeneous = true
+	if !cs.IsStandalone() {
+		cs.Heterogeneous = true
+	}
 	username, password := cs.Username, cs.Password
 	cs.Username = ""
 	cs.Password.Reset()
