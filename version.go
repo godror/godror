9df83c1109316838855cee7d3afc0315cd01e5f7
Tamás Gulácsi <tamas@gulacsi.eu>
2022-03-04 16:08:28 +0100

    bump version

--- version.go
+++ version.go
@@ -9,4 +9,4 @@ package godror
 //go:generate bash -c "echo main>odpi-version; set -x; curl -L https://github.com/oracle/odpi/archive/refs/heads/main.tar.gz | tar xzvf - odpi-$(cat odpi-version)/{embed,include,src,CONTRIBUTING.md,LICENSE.md,README.md} && cp -a odpi/embed/require.go odpi-$(cat odpi-version)/embed/ && cp -a odpi/include/require.go odpi-$(cat odpi-version)/include/ && cp -a odpi/src/require.go odpi-$(cat odpi-version)/src/ && rm -rf odpi && mv odpi-$(cat odpi-version) odpi; rm -f odpi-{,v}version; git checkout -- $(git status --porcelain -- odpi/*/*.go | sed -n -e '/ D / { s/^ D //;p;}')"
 
 // Version of this driver
-var Version = "v0.31.0"
+var Version = "v0.31.1"
