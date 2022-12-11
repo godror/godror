9f2dfd6cc378bb24ce9be4bde757d6851882f4f1
Tamás Gulácsi <tamas@gulacsi.eu>
2022-12-11 08:40:06 +0100

    Make staticcheck happy

--- z_test.go
+++ z_test.go
@@ -4244,14 +4244,14 @@ func TestExternalAuthIntegration(t *testing.T) {
 }
 
 func getRandomString() string {
-	rand.Seed(time.Now().UnixNano())
+	rnd := rand.New(rand.NewSource(time.Now().UnixNano()))
 	chars := []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
 		"abcdefghijklmnopqrstuvwxyz" +
 		"0123456789")
 	length := 8
 	var b strings.Builder
 	for i := 0; i < length; i++ {
-		b.WriteRune(chars[rand.Intn(len(chars))])
+		b.WriteRune(chars[rnd.Intn(len(chars))])
 	}
 	// fix starting letter as alphabet
 	return "A" + b.String() // E.g. "ExcbsVQs"
