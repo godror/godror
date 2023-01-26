8bbe6069727f46e3e416276f9dcfd1d58348891d
Tamás Gulácsi <T.Gulacsi@unosoft.hu>
2023-01-26 09:10:34 +0100

    update free.db wallet

--- contrib/free.db/env.sh
+++ contrib/free.db/env.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+export WALLET_PASSWORD=h2Y8eer53YY0NIoe
 echo '{ cd $(dirname $0); . env.sh; }' >&2
 export TNS_ADMIN="$(dirname "$(find "$(pwd)" -type f -name tnsnames.ora | sort -r | head -n1)")"
 export GODROR_TEST_DSN=user=test password=KSyB5a2vTpEKB1WT connectString=db201911301540_high standaloneConnection=1 timezone=UTC
