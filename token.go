2b8511c9080027d947b63e0c20423a9ae0df4e0f
sudarshan soma <sudarshan.ss.s@oracle.com>
2024-05-17 15:43:11 +0530

    - Add the token.go file

--- token.go
+++ token.go
@@ -6,11 +6,9 @@
 package godror
 
 /*
-#include <stdlib.h>
-#include <stdio.h>
+#include <stdint.h>
 #include "dpiImpl.h"
-
-int TokenCallbackHandlerDebug(void* context, dpiAccessToken *token);
+#include "token.h"
 
 */
 import "C"
@@ -18,7 +16,7 @@ import "C"
 import (
 	"context"
 	"fmt"
-	"sync"
+	"runtime/cgo"
 	"unsafe"
 
 	"github.com/godror/godror/dsn"
@@ -27,98 +25,70 @@ import (
 
 // AccessToken Callback information.
 type accessTokenCB struct {
-	ctx         context.Context
-	callback    func(context.Context, *dsn.AccessToken) error
-	id          *C.uint64_t
-	ctoken      *C.char
-	cprivateKey *C.char
+	ctx      context.Context
+	callback func(context.Context, *dsn.AccessToken) error
 }
-
-// Cannot pass *accessTokenCB to C, so pass an uint64 that points to
-// this map entry
-var (
-	accessTokenCBsMu sync.RWMutex
-	accessTokenCBs   = make(map[uint64]*accessTokenCB)
-	accessTokenCBsID uint64
-)
 
 // tokenCallbackHandler is the callback for C code on token expiry.
 //
 //export TokenCallbackHandler
-func TokenCallbackHandler(ctx unsafe.Pointer, accessToken *C.dpiAccessToken) {
+func TokenCallbackHandler(handle C.uintptr_t, accessToken *C.dpiAccessToken) {
+	h := (cgo.Handle)(handle)
+	tokenCB := h.Value().(accessTokenCB)
 	logger := getLogger(context.TODO())
 	if logger != nil && logger.Enabled(context.TODO(), slog.LevelDebug) {
-		logger.Debug("TokenCallbackHandler", "ctx", fmt.Sprintf("%p", ctx), "accessToken", accessToken)
+		logger.Debug("TokenCallbackHandler", "tokenCB", fmt.Sprintf("%p", tokenCB), "accessToken", accessToken)
 	}
-	accessTokenCBsMu.RLock()
-	tokenCB := accessTokenCBs[*((*uint64)(ctx))]
-	accessTokenCBsMu.RUnlock()
 
 	// Call user function which provides the new token and privateKey.
 	var refreshAccessToken dsn.AccessToken
 	if err := tokenCB.callback(tokenCB.ctx, &refreshAccessToken); err != nil {
 		if logger != nil && logger.Enabled(context.TODO(), slog.LevelDebug) {
-			logger.Debug("tokenCB.callback", "ctx", fmt.Sprintf("%p", ctx),
+			logger.Debug("tokenCB.callback", "callback", fmt.Sprintf("%p", tokenCB.callback),
 				"tokenCB", tokenCB)
 		}
 		return
-	}
-	if tokenCB.ctoken != nil {
-		C.free(unsafe.Pointer(tokenCB.ctoken))
-	}
-	if tokenCB.cprivateKey != nil {
-		C.free(unsafe.Pointer(tokenCB.cprivateKey))
 	}
 	token := refreshAccessToken.Token
 	privateKey := refreshAccessToken.PrivateKey
 	if token != "" {
 		accessToken.token = C.CString(token)
-		tokenCB.ctoken = accessToken.token
 		accessToken.tokenLength = C.uint32_t(len(token))
 		if privateKey != "" {
 			accessToken.privateKey = C.CString(privateKey)
-			tokenCB.cprivateKey = accessToken.privateKey
 			accessToken.privateKeyLength = C.uint32_t(len(privateKey))
 		}
 	}
 }
 
-// RegisterTokenCallback will add an entry of callback data in a map.
+// RegisterTokenCallback will populate the callback and context.
+// The void* datatype context is obtained by wrapping the cgo.Handle.
 func RegisterTokenCallback(poolCreateParams *C.dpiPoolCreateParams,
 	tokenGenFn func(context.Context, *dsn.AccessToken) error,
-	tokenCtx context.Context, id *uint64) {
-	// typedef int (*dpiAccessTokenCallback)(void* context, dpiAccessToken *accessToken);
-	poolCreateParams.accessTokenCallback = C.dpiAccessTokenCallback(C.TokenCallbackHandlerDebug)
+	tokenCtx context.Context) unsafe.Pointer {
 
-	// cannot pass &accessTokenCB to C, so pass indirectly
+	// typedef int (*dpiAccessTokenCallback)(void* context, dpiAccessToken *accessToken);
+	poolCreateParams.accessTokenCallback = C.dpiAccessTokenCallback(C.godrorTokenCallbackHandlerDebug)
 	tokenCB := accessTokenCB{callback: tokenGenFn, ctx: tokenCtx}
-	accessTokenCBsMu.Lock()
-	accessTokenCBsID++
-	*id = accessTokenCBsID
-	accessTokenCBs[accessTokenCBsID] = &tokenCB
-	accessTokenCBsMu.Unlock()
-	tokenID := (*C.uint64_t)(C.malloc(8))
-	tokenCB.id = tokenID
-	*tokenID = C.uint64_t(accessTokenCBsID)
-	poolCreateParams.accessTokenCallbackContext = unsafe.Pointer(tokenID)
+	h := cgo.NewHandle(tokenCB)
+	wctx := C.godrorWrapHandle(C.uintptr_t(h))
+	poolCreateParams.accessTokenCallbackContext = unsafe.Pointer(wctx)
+	return unsafe.Pointer(wctx)
 }
 
-// UnRegisterTokenCallback will remove the token callback data registered
+// UnRegisterTokenCallback will free the token callback data registered
 // during pool creation.
-func UnRegisterTokenCallback(key uint64) {
-	accessTokenCBsMu.Lock()
-	value, ok := accessTokenCBs[key]
-	defer accessTokenCBsMu.Unlock()
-	if ok {
-		if value.id != nil {
-			C.free(unsafe.Pointer(value.id))
+func UnRegisterTokenCallback(ptr unsafe.Pointer) {
+	if ptr != nil {
+		ctx := (*C.godrorHwrap)(ptr)
+		h := cgo.Handle(ctx.handle)
+		if ctx.token != nil {
+			C.free(unsafe.Pointer(ctx.token))
 		}
-		if value.ctoken != nil {
-			C.free(unsafe.Pointer(value.ctoken))
+		if ctx.privateKey != nil {
+			C.free(unsafe.Pointer(ctx.privateKey))
 		}
-		if value.cprivateKey != nil {
-			C.free(unsafe.Pointer(value.cprivateKey))
-		}
-		delete(accessTokenCBs, key)
+		h.Delete()
+		C.free(ptr)
 	}
 }
