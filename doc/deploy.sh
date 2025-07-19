a0f899d23eca6d2b974f219024e9acb7f391d12b
Tamás Gulácsi <tamas@gulacsi.eu>
2025-07-19 11:58:34 +0200

    doc/deploy.sh: deploy all version, not just the tagged

--- doc/deploy.sh
+++ doc/deploy.sh
@@ -1,14 +1,25 @@
-#!/bin/sh
-set -eu
-git checkout gh-pages
-trap 'git checkout main' EXIT 
+#!/bin/bash
+set -euo pipefail
+if ! git worktree list | grep -F -q '[gh-pages]'; then
+	git worktree add --checkout gh-page gh-pages
+fi
+(
+cd gh-pages 
 git pull -s recursive -X theirs origin gh-pages
-version="$(git tag | sort -Vr|head -n1)"
-set -x
-files=$(git show "$version":./doc | sed -ne '/[.]md$/ s,^,./doc/,p'; git show "$version":./ | grep '[.]md$')
-git checkout "$version" -- $files
-git add $files
+read -r hsh tag < <(git log --format='%H %(decorate:tag=)' main | head -n1)
+version="$(echo "$tag" | sed -ne '/^(v/ { s/[()]//g; p; }')"
+if [[ -z "$version" ]]; then
+	version="$hsh"
+fi
+
+declare -a files
+while read -r fn; do
+	files+=("$fn")
+done < <(git show "$hsh":./doc | sed -ne '/[.]md$/ s,^,./doc/,p'; git show "$hsh":./ | grep '[.]md$')
+git checkout "$version" -- "${files[@]}"
+git add "${files[@]}"
 if git status --porcelain | grep -q '^[ MAD]'; then
 	git commit -am "Update gh-pages to $version" -n
 	git push origin gh-pages
 fi
+)
